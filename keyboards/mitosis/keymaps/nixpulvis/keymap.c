d1fd6cfba22aec0c572b8ad2d0349e7778157ccb
Nathan Lilienthal <nathan@nixpulvis.com>
2018-11-10 15:21:54 -0500

    Better layout for this maddening thing.

--- keyboards/mitosis/keymaps/nixpulvis/keymap.c
+++ keyboards/mitosis/keymaps/nixpulvis/keymap.c
@@ -7,6 +7,7 @@ enum mitosis_layers
   _UPPER,
   _CORNER,
   _FUNC,
+  _ARROW,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -15,17 +16,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______,
 
-            _______, KC_LALT, KC_LCTL, TT(_UPPER),   TT(_CORNER), KC_RCTL, KC_DEL,  _______,
-            KC_ESC,  KC_LGUI, KC_TAB,  _______,      _______,     KC_ENT,  KC_BSPC, TT(_FUNC)
-  ),
+    KC_LGUI,    KC_LALT,    KC_LCTL,     KC_TAB,    KC_ESC,  KC_RCTL, KC_DEL,  _______,
+    TG(_ARROW), MO(_UPPER), MO(_CORNER), _______,   _______, KC_ENT, KC_BSPC, _______
+    ),
 
   [_QWERTY] = LAYOUT(
-    KC_Q,        KC_W,   KC_E,   KC_R,   KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
-    KC_A,        KC_S,   KC_D,   KC_F,   KC_G,      KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,
-    SFT_T(KC_Z), KC_X,   KC_C,   KC_V,   KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT, SFT_T(KC_SLSH),
+      KC_Q,        KC_W,   KC_E,   KC_R,   KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
+      KC_A,        KC_S,   KC_D,   KC_F,   KC_G,      KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,
+      SFT_T(KC_Z), KC_X,   KC_C,   KC_V,   KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT, SFT_T(KC_SLSH),
 
-            KC_GRV,  _______, _______, _______,     _______, _______, _______, KC_BSLS,
-            _______, _______, _______, KC_SPC,      KC_SPC,  _______, _______, _______
+            _______,  _______, _______, _______,      _______, _______, _______, KC_BSLS,
+            _______, _______, _______,  KC_SPC,        KC_SPC,  _______, _______,  KC_GRV
   ),
 
   [_UPPER] = LAYOUT(
@@ -54,6 +55,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   ),
+
+  [_ARROW] = LAYOUT(
+    XXXXXXX, XXXXXXX, KC_UP, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_LSFT, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            _______, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+  )
 };
 
 void matrix_init_user(void)
